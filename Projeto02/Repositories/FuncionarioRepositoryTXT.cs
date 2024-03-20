682c543115251d97673ce3a42a06cc687df6e2b6
LucasMMF <mateusmidao@gmail.com>
2024-03-19 23:34:00 -0300

    feat: incluindo abstratação FuncionarioRepository
    inclui a classe abstrata FuncionarioRepository,
    bem como duas especializações, em TXT e CSV,
    desta classe. Que herdam dessa abstração.

---
+++ Projeto02/Repositories/FuncionarioRepositoryTXT.cs
@@ -0,0 +1,18 @@
+﻿using Projeto02.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Repositories
+{
+    public class FuncionarioRepositoryTXT : FuncionarioRepository
+    {
+        public override void ExportarDados(Funcionario funcionario)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
